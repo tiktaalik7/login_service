a3104c232ba8007e739e7e37d998700954a984a7
minseo Cheon <tiktaalik76@daum.net>
2023-02-22 13:52:53 +0900

    change class name system to loginsystem

--- src/main/java/ten/LoginSystem.java
+++ src/main/java/ten/LoginSystem.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.Set;
 
-public class System {
+public class LoginSystem {
     Database mDatabase = Database.getInstance();
     private static final int EXISTED = 0;
     private static final int SUCCESS = 1;
